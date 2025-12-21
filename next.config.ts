452ad2ebe9774f166f03ad8c99821d7eddb9da1f
Pardi Pardi <pardi.jhr@gmail.com>
2025-12-21 10:40:52 +0000

    ganti logo menjadi : https://bantuanbiayapendidikan.medan.go.id/public/i

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'bantuanbiayapendidikan.medan.go.id',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
--- src/components/app/Logo.tsx
+++ src/components/app/Logo.tsx
@@ -1,18 +1,13 @@
-export function Logo(props: React.SVGProps<SVGSVGElement>) {
+import Image from "next/image";
+
+export function Logo(props: Omit<React.ComponentProps<typeof Image>, 'src' | 'alt'>) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+    <Image
+      src="https://bantuanbiayapendidikan.medan.go.id/public/images/pemko%20medan.png"
+      alt="Logo Pemko Medan"
+      width={40}
+      height={40}
       {...props}
-    >
-      <path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z" />
-      <path d="M12 6v6l4 2" />
-      <path d="m16 10-4 4" />
-    </svg>
+    />
   );
 }
